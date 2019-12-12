42e3047335f0e323f1df1f015c1e918f98bf6d85
kellysciandra <kellysciandra@icloud.com>
2019-12-12 10:31:28 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
 // Code MovieReviews Here
+import React from 'react';
+// import LatestMovieReviewsContainer from './LatestMovieReviewsContainer'
+// import SearchableMovieReviewsContainer from './SearchableMovieReviewsContainer'
+
+
+const Review = ({
+    headline, byline, link, summary_short
+}) => {
+    return(
+        <div 
+        key={headline}
+        className="review"></div>
+    )
+}
+
+const MovieReviews = ({reviews}) => 
+    <div className="review-list"> 
+    {reviews.map(Review)}
+    </div>
+
+
+export default MovieReviews
