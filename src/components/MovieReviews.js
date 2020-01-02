d11e83e1d797fd532efe84a2c00539a6aa71818c
dsstewa <davidstewart@gmail.com>
2020-01-01 23:08:21 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,19 +1,17 @@
 // Code MovieReviews Here
 import React from 'react'
 
-const MovieReviews = (props) => {  
-    const rendList = props.reviews.map((review, index) => {
-        return (
-            <div className="review" key={index}>
-                <a href={review.link.url}>{review.headline}</a><br/>
-            </div>
-        )
-    })
-        return ( 
-            <div className="review-list" >
-                {rendList} 
-            </div>           
-        )               
-}
+const MovieReviews = (props) => {
+   
+   
+   
+   return (
+        <div>
+            {console.log(props)}
+        </div>
+         )
+    }
 
 export default MovieReviews
+
+
