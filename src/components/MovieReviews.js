568b7067b4de3977f4211549b99bab7ddcfebf37
Ryan <kerneltux0@gmail.com>
2020-01-06 09:19:29 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,11 +3,10 @@ import React from 'react';
 
 const MovieReviews = ({review}) => (
   <div className='review-list'>
-    <div className='review'>
-      <h2>{review.headline}</h2>
+    <div key={review.headline} className='review'>
       <h3>{review.byLine}</h3>
       <h4>{review.summary_short}</h4>
-      <a href={review.link.url}>Link to story</a>
+      <a href={review.link.url}>{review.headline}</a>
     </div>
   </div>
 )
